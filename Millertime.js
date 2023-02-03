aaa64bbf86fab09bf96f04b0eab8c3fb5d9f7228
jakenobles <jakenobles123@gmail.com>
2023-02-02 22:56:04 -0600

    first commit

---
+++ Millertime.js
@@ -0,0 +1,41 @@
+function get_time() {
+    // finds the time elapsed between the set date and now and scales that to the time dilation of 1 hour to 7 years
+
+    const millerlanding = new Date(2014, 9, 26, 0, 0, 0);
+    
+    const currenttime = new Date();
+
+    // remember that this is in units of milliseconds
+    const time_passed = currenttime - millerlanding;
+
+    const seconds_passed = time_passed / 1000;
+
+    const scaled_time = seconds_passed * 0.163e-4;
+
+    return scaled_time;
+}
+
+function sort_time_data() {
+
+
+    let days = divmod(get_time(), 86400);
+    let hours   = divmod(days[1], 3600);
+    let minutes = divmod(hours[1], 60);
+    let seconds = divmod(minutes[1], 1);
+    let milliseconds = divmod(seconds[1], .001);
+
+    document.getElementById("test1").innerHTML = days[0] + ' days, ' + hours[0] + ' hours, ' 
+    + minutes[0] + ' minutes, ' + seconds[0] + ' seconds and ' + milliseconds[0] + ' milliseconds';
+}
+
+function divmod(x, y){
+    //Writing my own divmod function like exists in python.
+    let div1 = Math.floor(x/y);
+    let div2 = x%y;
+    const divmodarray = [div1, div2];
+    return divmodarray;
+}
+
+function looper() {
+    setInterval(sort_time_data, 500);
+}
